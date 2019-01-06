4dd0b65822efe5bd0298ffebd9f3963177b526f5
Gitea <gitea@fake.local>
2019-01-06 11:42:31 +0100

    How to get the absolut dir name in a bash script?

---
+++ bash_script/get_path_value.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# relative path
+dir1=`dirname $0`
+
+# absolute path
+dir2=`dirname $(realpath $0)`
+dir3="$( cd "$(dirname "$0")" ; pwd -P )"
+
+echo -e "d1=$dir1\nd2=$dir2\nd3=$dir3"
